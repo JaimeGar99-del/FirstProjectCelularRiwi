9a85471baf4330d9521f95aba12f76ec32fa8b6d
Andres Elles <andreselles1107@gmail.com>
2026-02-26 07:45:53 -0500

    actualice rama feature/123-solicitud-de-datos

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Factura")
+
+print("Datos del cliente")
+
+nombre_del_vendedor = input("ingresa nombre del vendedor")
+
+nombre_del_cliente = input("Ingresa nombre del cliente")
+
+dia=input("ingresa dia de la venta")
+mes = input("ingresa mes de la venta")
+año = input(" ingresa año de la venta")
+
+
+            
+
+
+
